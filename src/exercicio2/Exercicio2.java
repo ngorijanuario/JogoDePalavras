c5cea600eebe94797c9393923c7cc05afbf86a49
Ngori Januário <68328526+ngorijanuario@users.noreply.github.com>
2022-06-05 20:13:05 +0100

    Versão beta para testes

---
+++ src/exercicio2/Exercicio2.java
@@ -0,0 +1,8 @@
+package exercicio2;
+//@author Ngori Januário
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Es n = new Es();
+        n.setVisible(true);
+    }
+}
